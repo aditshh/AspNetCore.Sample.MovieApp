a4f155cac045defdd82812839f598ede8c4caec6
Adit Shah <aditshh@gmail.com>
2019-03-23 10:25:44 -0400

    Add project files.

---
+++ AspNetCore.Sample.MovieApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCore.Sample.MovieApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
