d54b755d9ee6fe843e109e45a409be9f86a1b639
Adit Shah <aditshh@gmail.com>
2019-03-24 01:12:16 -0400

    Implementation of App to work with database (code-first).

--- AspNetCore.Sample.MovieApp/Program.cs
+++ AspNetCore.Sample.MovieApp/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using AspNetCore.Sample.MovieApp.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCore.Sample.MovieApp
 {
@@ -14,7 +12,26 @@ namespace AspNetCore.Sample.MovieApp
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<AspNetCoreSampleMovieAppContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- AspNetCore.Sample.MovieApp/Startup.cs
+++ AspNetCore.Sample.MovieApp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using AspNetCore.Sample.MovieApp.Models;
 
 namespace AspNetCore.Sample.MovieApp
 {
@@ -33,6 +35,9 @@ namespace AspNetCore.Sample.MovieApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<AspNetCoreSampleMovieAppContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("AspNetCoreSampleMovieAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
